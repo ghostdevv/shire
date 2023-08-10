2f9b06b478f3535b2bfd0c52c21018a9c5a59b64
GHOST <ghostdevbusiness@gmail.com>
2023-08-10 18:32:17 +0100

    fix: trim the ip

--- src/ip.rs
+++ src/ip.rs
@@ -3,12 +3,14 @@ use color_eyre::eyre::Result;
 pub async fn get_ip() -> Result<String> {
     let client = reqwest::Client::new();
 
-    let response = client
+    let ip = client
         .get("https://ip.willow.sh")
         .send()
         .await?
         .text()
-        .await?;
+        .await?
+        .trim()
+        .to_owned();
 
-    Ok(response)
+    Ok(ip)
 }

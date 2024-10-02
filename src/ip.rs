7ca91ac0a20372a4c28202b9ef0652d5c4dd20a7
GHOST <ghostdevbusiness@gmail.com>
2024-10-02 09:39:12 +0100

    fix: check response status code
    Closes #13

--- src/ip.rs
+++ src/ip.rs
@@ -1,18 +1,24 @@
 use crate::utils;
-use color_eyre::eyre::Result;
+use color_eyre::eyre::{eyre, Result};
 
 pub async fn get_ip(resolver: &str) -> Result<String> {
     let client = reqwest::Client::new();
 
-    let ip = client
+    let response = client
         .get(resolver)
         .header("User-Agent", utils::get_ua_header())
         .send()
-        .await?
-        .text()
-        .await?
-        .trim()
-        .to_owned();
+        .await?;
+
+    // Check if the status code is 200
+    if response.status() != reqwest::StatusCode::OK {
+        return Err(eyre!(
+            "The ip resolver failed with status code: {}",
+            response.status()
+        ));
+    }
+
+    let ip = response.text().await?.trim().to_owned();
 
     Ok(ip)
 }
